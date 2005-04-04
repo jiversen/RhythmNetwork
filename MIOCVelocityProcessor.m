5e1d39822198fae3b14bdfe23017d49011bdebc5
jive <jive@93fffc33-0af3-0310-b10e-99e8aafd6047>
2005-04-04 05:11:35 +0000

    RN: Initial definition of MIOCVelocityProcessor, update diary
    git-svn-id: file:///Users/Shared/svn/OSX/RhythmNetwork/trunk@21 93fffc33-0af3-0310-b10e-99e8aafd6047

---
+++ MIOCVelocityProcessor.m
@@ -0,0 +1,14 @@
+//
+//  MIOCVelocityProcessor.m
+//  RhythmNetwork
+//
+//  Created by John Iversen on 4/4/05.
+//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//
+
+#import "MIOCVelocityProcessor.h"
+
+
+@implementation MIOCVelocityProcessor
+
+@end
